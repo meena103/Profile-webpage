7a4782f3f0d9d443be88091065342bd9a5138eba
Vaibhav Meena <meena.40@iitj.ac.in>
2022-11-15 19:26:20 +0530

    update

--- myblogs/settings.py
+++ myblogs/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 ...
 # All of your settings here
 ...
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,6 +103,7 @@ DATABASES = {
     }
 }
 
+django_heroku.settings(locals())
 
 
 # Password validation
